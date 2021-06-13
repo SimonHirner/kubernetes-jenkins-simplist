726bfea61a603d9977faf1cdb1c69a4df2a583dc
SimonHirner <simon.hirner@hm.edu>
2021-06-13 19:35:19 +0200

    Deactivate spring boot auto configuration for MongoDB.

--- simplist/backend/src/main/java/edu/hm/simplist/SimplistApplication.java
+++ simplist/backend/src/main/java/edu/hm/simplist/SimplistApplication.java
@@ -5,9 +5,12 @@ import edu.hm.simplist.model.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SimplistApplication implements CommandLineRunner {
 
 	private final TaskRepository taskRepository;
